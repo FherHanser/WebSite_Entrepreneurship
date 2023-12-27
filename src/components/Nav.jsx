118ac11d51e51a2253f01fcb5c357a44762d203c
Fernand J. Hanser <hanstromb@gmail.com>
2023-12-27 12:31:35 -0600

    refacto component nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
-// Nav.js
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './style.css'; // Asegúrate de importar tu archivo de estilos
+import './style.css'; // Asegúrate de importar tu archivo CSS
 
 function Nav() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   useEffect(() => {
     // Restablecer los estilos del nav al cargar la página
     resetNavStyles();
@@ -12,6 +13,8 @@ function Nav() {
     const handleRouteChange = () => {
       // Restablecer los estilos del nav al cambiar de ruta
       resetNavStyles();
+      // Cierra el menú móvil después de hacer clic en un enlace durante la navegación
+      setMobileMenuOpen(false);
     };
 
     // Agregar el listener al evento de cambio de ruta
@@ -27,9 +30,7 @@ function Nav() {
     const nav = document.querySelector('nav');
     if (nav) {
       // Restablecer estilos del nav
-      nav.style.flexDirection = 'row';
-      nav.style.alignItems = 'center';
-      nav.style.justifyContent = 'center';
+      nav.style.top = '120px'; // Ajusta el valor según sea necesario
     }
   };
 
@@ -41,32 +42,43 @@ function Nav() {
     });
   };
 
+  const handleMobileMenuToggle = () => {
+    // Invierte el estado del menú móvil al hacer clic en el botón hamburguesa
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav>
-      <Link to="/" onClick={handleNavClick}>
-        Inicio
-      </Link>
-      <Link to="/QuienesSomos" onClick={handleNavClick}>
-        Quienes Somos
-      </Link>
-      <Link to="/Servicios" onClick={handleNavClick}>
-        Servicios
-      </Link>
-      <Link to="/Cursos" onClick={handleNavClick}>
-        Cursos
-      </Link>
-      <Link to="/Alianzas" onClick={handleNavClick}>
-        Alianzas
-     </Link>
-     <Link to="/Market" onClick={handleNavClick}>
-        Market
-     </Link>
-     <Link to="/Revista" onClick={handleNavClick}>
-        Revista
+      <div className="mobile-menu-toggle" onClick={handleMobileMenuToggle}>
+        &#9776; {/* Icono de hamburguesa */}
+      </div>
+
+      <div className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={handleNavClick}>
+          Inicio
         </Link>
-      <Link to="/Contacto" onClick={handleNavClick}>
-        Contacto
-     </Link>
+        <Link to="/QuienesSomos" onClick={handleNavClick}>
+          Quienes Somos
+        </Link>
+        <Link to="/Servicios" onClick={handleNavClick}>
+          Servicios
+        </Link>
+        <Link to="/Cursos" onClick={handleNavClick}>
+          Cursos
+        </Link>
+        <Link to="/Alianzas" onClick={handleNavClick}>
+          Alianzas
+        </Link>
+        <Link to="/Market" onClick={handleNavClick}>
+          Market
+        </Link>
+        <Link to="/Revista" onClick={handleNavClick}>
+          Revista
+        </Link>
+        <Link to="/Contacto" onClick={handleNavClick}>
+          Contacto
+        </Link>
+      </div>
     </nav>
   );
 }
